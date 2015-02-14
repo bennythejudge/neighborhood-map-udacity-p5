7654f42c300fc361e040423e6a05ed051231cd0a
bennythejudge <logube@gmail.com>
2015-02-14 22:56:46 +0000

    fitBounds now seems ok

--- js/main.js
+++ js/main.js
@@ -9,16 +9,22 @@ $(document).ready(function() {
         'use strict';
         /** save the "this" for when the context changes */
         var self = this;
+        /** 
+         * create a single infowindow object to be reused - according to google the right
+         * way to have only one infowindow open at any given time
+         */
+        var infoWindow = new google.maps.InfoWindow();
+        self.bounds = new google.maps.LatLngBounds();
         self.locations = ko.observableArray([
-                {cat: 'culture', name: 'Barbican Centre', address: 'EC2Y 8DS', city: 'london', lat: 51.5204543, long: -0.0937136999999666, description: '', url: '', img: ''},
-                {cat: 'culture', name: 'Barbican Cinemas', address: 'Beech Street', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'companies', name: 'Amazon Development Centre', address: 'EC1A 4JU', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'places', name: 'Silicon Roundabout', address: 'EC1Y 1BE', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'places', name: 'Shoreditch High Street station', address: 'Shoreditch High Street station', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'stores', name: 'Apple store', address: 'W1B 2EL', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'companies', name: 'Google', address: 'SW1W 9tq', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'stores', name: 'Boxpark shoreditch', address: 'E1 6gy', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
-                {cat: 'events', name: 'Silicon Milkroundabout', address: 'E1 6QL', city: 'london', lat: NaN, long: NaN, description: '', url: '', img: ''},
+                {cat: 'culture', name: 'Barbican Centre', address: 'EC2Y 8DS', city: 'london', lat: 51.5204543, long: -0.0937136999999666, description: 'Great venue for music, cinema and exhibitions.', url: 'http://www.barbican.org.uk/', img: ''},
+                {cat: 'companies', name: 'Google', address: 'SW1W 9tq', city: 'london', lat: 51.49496560000001, long: -0.14667389999999614, description: 'The Mothership', url: 'https://www.google.com', img: ''},
+                {cat: 'culture', name: 'Barbican Cinemas Cafe', address: 'Beech Street', city: 'london', lat: 51.5205906, long: -0.09486970000000383, description: '', url: '', img: ''},
+                {cat: 'companies', name: 'Amazon Development Centre', address: 'EC1A 4JU', city: 'london', lat: 51.5216718, long: -0.09826629999997749, description: '', url: '', img: ''},
+                {cat: 'places', name: 'Silicon Roundabout', address: 'EC1Y 1BE', city: 'london', lat: 51.52567029999999, long: -0.08756149999999252, description: '', url: '', img: ''},
+                {cat: 'places', name: 'Shoreditch High Street station', address: 'Shoreditch High Street station', city: 'london', lat: 51.52338, long: -0.07521999999994478, description: '', url: '', img: ''},
+                {cat: 'stores', name: 'Apple store', address: 'W1B 2EL', city: 'london', lat: 51.5142651, long: -0.14222989999996116, description: 'First store to open in Europe in 2004', url: 'http://tinyurl.com/kwo7qnz', img: ''},
+                {cat: 'stores', name: 'Boxpark shoreditch', address: 'E1 6gy', city: 'london', lat: 51.52338109999999, long: -0.07573070000000826, description: '', url: '', img: ''},
+                {cat: 'events', name: 'Silicon Milkroundabout', address: 'E1 6QL', city: 'london', lat: 51.5217064,long:  -0.0722892999999658, description: '', url: '', img: ''},
         ]);
         self.geolocations = ko.observableArray([]);
         /** 
@@ -32,8 +38,9 @@ $(document).ready(function() {
                 geocoder.geocode( { 'address': locaddress}, function(results, status) {
                       if (status == google.maps.GeocoderStatus.OK) {
                           var latlng =  new google.maps.LatLng(results[0].geometry.location['k'], results[0].geometry.location['D']);
-                          // console.log('geocoding: ' + status + ' ' + latlng);
-                          addMarker(latlng);
+                          console.log('geocoding: ' + status + ' ' + latlng + ' ' + location.name);
+                          addMarker(latlng,location);
+                          // console.log(location.name);
                           // add to geolocation observablearray
                           self.geolocations().push({name: location.name, latlng: latlng});
                       } else {
@@ -42,30 +49,49 @@ $(document).ready(function() {
                 });
             });
         };
-    
-        function addMarker(latlng) {
+        function addMarker(latlng,location) {
             // console.log(self.geolocations());
+            var currentMarker;
             var marker = new google.maps.Marker({
-                        map: self.map,
-                        animation: google.maps.Animation.DROP,
-                        position: latlng,
+                map: self.map,
+                animation: google.maps.Animation.DROP,
+                position: latlng,
+                title: location.name,
             });
             self.bounds.extend(latlng);
+            self.map.fitBounds(self.bounds);
+            self.map.setCenter(new google.maps.LatLng(self.locations()[0].lat, self.locations()[0].long));
+            var contentString = 
+                '<div id="content">'+
+                '<h2>'+location.name+'</h2>'+
+                '<div id="bodyContent">'+
+                '<p>'+location.description+'</p>'+
+                '<a href="'+location.url+'" target="_blank">'+location.url+'</a>'+
+                '</div>'+
+                '</div>';
+            // console.log(contentString);
+            google.maps.event.addListener(marker, 'click', function() {
+                  infoWindow.setContent(contentString);
+                  infoWindow.open(self.map,this);
+                  currentMarker = this;
+            });
         };
-    
         /** 
          * IIFE so it's executed immediately
          */
         var initMap = function() {
-            geocoding();
+            //geocoding();
             self.map = new google.maps.Map(document.getElementById('map-canvas'), {
                 zoom: 15,
                 center: new google.maps.LatLng(self.locations()[0].lat, self.locations()[0].long),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             });
-            self.bounds = new google.maps.LatLngBounds();
+            ko.utils.arrayForEach (self.locations(), function(location) {
+                var latlng =  new google.maps.LatLng(location.lat,location.long);
+                console.log(location.name);
+                addMarker(latlng,location);
+            });
         }();
-    
         /**
          * update map bounds on page resize
          */
